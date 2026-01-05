9f4d111892a7c789cf97e92db9ef3c7fd85eed76
Peter Steinberger <steipete@gmail.com>
2026-01-05 18:48:09 +0100

    style(web): soften footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,14 @@ export function Footer() {
         <div className="site-footer-divider" aria-hidden="true" />
         <div className="site-footer-row">
           <div className="site-footer-copy">
-            Open source (MIT) on{' '}
+            <a href="https://clawdbot.com" target="_blank" rel="noreferrer">
+              Clawdbot
+            </a>{' '}
+            ·{' '}
             <a href="https://github.com/clawdbot/clawdhub" target="_blank" rel="noreferrer">
-              GitHub
-            </a>
-            . Made by{' '}
+              Open source (MIT)
+            </a>{' '}
+            · Made by{' '}
             <a href="https://steipete.me" target="_blank" rel="noreferrer">
               Peter Steinberger
             </a>
